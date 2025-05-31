334219012cf5673a9d833488d41ffa1876c213b7
Karoline Andrade <karolineandrade47@gmail.com>
2025-05-31 12:30:48 -0400

    :sparkles: feat: add route to post list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "posts"
+  },
+  {
+    path: "posts",
+    loadComponent: () => import("./posts/posts.component").then(m => m.PostsComponent),
+  },
+]
