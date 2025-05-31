ab2bbf83c1edca7ff9c2b159bc878335d10136af
Karoline Andrade <karolineandrade47@gmail.com>
2025-05-30 23:52:29 -0400

    :wrench: chore: add  environment

---
+++ src/environments/environments.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  appName: 'Posts',
+  apiUrl: 'https://jsonplaceholder.typicode.com/',
+}
---
+++ src/environments/environments.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  appName: 'Posts',
+  apiUrl: 'https://jsonplaceholder.typicode.com/',
+}
