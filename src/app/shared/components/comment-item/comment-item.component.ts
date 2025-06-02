531cd8f0dfe20f0a5e54edf8ce94144f429b36f5
Karoline Andrade <karolineandrade47@gmail.com>
2025-06-01 20:05:37 -0400

    :sparkles: feat: add comment-item component

---
+++ src/app/shared/components/comment-item/comment-item.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, input, OnInit, Output } from '@angular/core';
+import { TuiAutoColorPipe, TuiButton, TuiIcon } from '@taiga-ui/core';
+import { TuiAvatar } from '@taiga-ui/kit';
+import { CommentInterface } from '../../interface/CommentInterface';
+
+@Component({
+  selector: 'app-comment-item',
+  templateUrl: './comment-item.component.html',
+  styleUrls: ['./comment-item.component.css'],
+  imports: [TuiAvatar, TuiAutoColorPipe, TuiIcon, TuiButton]
+})
+export class CommentItemComponent {
+  public comment = input<CommentInterface>();
+  @Output() onEdit = new EventEmitter<CommentInterface>();
+  @Output() onDelete = new EventEmitter<number >();
+
+  constructor() { }
+
+  editComment(): void {
+    this.onEdit.emit(this.comment());
+  }
+
+   deleteComment(index: number): void {
+    this.onDelete.emit(index);
+  }
+  getFirstChar(fullName: string): string {
+  return fullName.charAt(0).toUpperCase();
+}
+}
--- src/app/shared/service/storage.service.ts
+++ src/app/shared/service/storage.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class StorageService {
-
 constructor() { }
 
 saveToStorage(key: string, value: string): void {
